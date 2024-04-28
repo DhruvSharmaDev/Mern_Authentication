ad4a0f3f04073f63f74bff4171171a33d4c3383e
Dhruvsharma <sharmadhruv1303@gmail.com>
2024-04-28 11:39:20 +0530

    create SignUp Api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
+dotenv.config()
+// never use special characcter in the password
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("MongoDb connected")
+}).catch((err)=>{
+    console.log(err.message);
+
+})
+
 const app=express();
+app.use(express.json())
+
+app.use("/api/user",userRoutes)
+app.use("/api/Auth",authRoutes)
 app.listen(3000,()=>{
     console.log("Server is listening at the port number 3000")
 })
